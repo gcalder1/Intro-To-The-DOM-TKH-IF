b42bf85b4bb88b19478ddf2aeb3db90611d8774e
unknown <george.calderon2000@gmail.com>
2025-04-01 19:34:27 -0400

    querySelector exercise
    had to troubleshoot the querySelector thing a bit because couldnt understand but it shows now on the console
    
    Fixes-Bug: n/a
    Implements-Requirement: n/a

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const title = document.getElementById("title");
 
 console.log(title);
+
+const header2 = document.querySelector("#good");
+const p1 = document.querySelector("#neat");
+const p2 = document.querySelector("#whoa");
+
+
+console.log(header2);
+console.log(p1);
+console.log(p2);
