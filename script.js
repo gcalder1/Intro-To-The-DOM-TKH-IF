39312c4f7a350f58540fd20b93ef37ef956019cd
unknown <george.calderon2000@gmail.com>
2025-04-02 02:19:29 -0400

    Added and Saved my work for 4/1/2025 exercises
    I forgot to save my work throughout the class while doing these exercises,
    so I made sure to finish and write down my thoughts on how certain things
    worked so that I wont forget what they do. I explain my thoughts on what each
    thing I wrote does along with why I think it does what it does. All the
    exercises in the .JS for this commit are what were covered in class on
    4/1/2025 between 5:30PM and 9:30PM. Continued work will appear untill
    ALL exercises for the 4/1/2025 slides are covered and completed.
    
    Fixes-Bug: n/a
    Implements-Requirement: n/a

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//getElementByID
 const title = document.getElementById("title");
 
 console.log(title);
 
+
+//querySelector
 const header2 = document.querySelector("#good");
 const p1 = document.querySelector("#neat");
 const p2 = document.querySelector("#whoa");
@@ -11,6 +14,48 @@ console.log(header2);
 console.log(p1);
 console.log(p2);
 
-const ptags = document.querySelectorAll ("p");
+
+//querySelectorAll
+const ptags = document.querySelectorAll ("p"); //yields node list (≈ array)
 
 console.log(ptags);
+
+
+//innerText
+const h3Boom = document.querySelector(".lebomby");
+
+console.log(h3Boom.innerText);//before changes made
+
+h3Boom.innerText = "no more boom ):"; //single element target means can use innerText to solely target that element
+
+console.log(h3Boom.innerText);//after changes made
+
+const h3Tags = document.querySelectorAll(".kaboom");
+
+h3Tags.forEach(h3Tags => {
+    console.log(h3Tags.innerText); //saying to show what h3Tags= in terms of it's innerText
+    h3Tags.innerText = "Le Bombino!";
+    console.log(h3Tags.innerText);
+}); //need .forEach because node list ≈ array, need to look innerText through each to change since can't target all with querySelectorAll
+
+
+//changing stuff with css via JS
+
+const modifyTextParagraph = document.querySelector("p#modifyTextParagraph");
+
+modifyTextParagraph.style.fontSize = "30px"; //use camelCase for css props
+modifyTextParagraph.style.color = "blue";
+
+const img = document.querySelector("#shrek"); //define variable for img
+
+img.setAttribute("src", "https://static.wikia.nocookie.net/universalstudios/images/f/f2/Shrek2-disneyscreencaps.com-4369.jpg/");
+//this targets the src and inserts that link / this needs two arguments because one is the name of the attribute you want to change while the other is a new value for that argument
+//ALWAYS needs both to work / both need to be strings because html is text based, therefore making JS required to use strings for both arguments since thats the only thing setAttribute will recognize to change within the html
+
+
+//creating elements using JS via DOM
+const paragraphCreated = document.createElement("p"); //p was created thanks to .createElement but exists in the literal void
+
+document.body.append(paragraphCreated); //append is how it appears within the DOM (check dev elements)
+
+paragraphCreated.innerText = "it's that shrimple"; //fills the text, and then we can see it within the html output on live server
